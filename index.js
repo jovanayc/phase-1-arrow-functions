68897074c9fca2eddd9c7a8f042ee95822ec76f8
Jovanay <jovanay.d.carter@gmail.com>
2022-01-11 22:31:12 -0500

    add existing file

---
+++ index.js
@@ -0,0 +1,19 @@
+let divide = function (){
+  let resultA = 2000/100;
+  return resultA;
+  }
+
+let square = a => {
+  let resultB = a * a;
+  return resultB;
+  }
+
+let add = (x,y) => {
+  let resultC = x + y;
+  return resultC;
+  }
+
+  console.log(divide());
+  console.log(square(9));
+  console.log(add(2,5));
+  
